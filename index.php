3d16fca66ea5b0c5475b930558902af868728332
sinca85 <sinca85@gmail.com>
2021-07-12 15:49:22 -0300

    first commit

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * @package am750
  */
 // refe https://coronavirus.msal.gov.ar/vacunas/d/8wdHBOsMk/seguimiento-vacunacion-covid/d/8wdHBOsMk/seguimiento-vacunacion-covid?orgId=1&refresh=15m%3F%3F%3F%3F%3F%3Flogin
-get_header();
+//get_header();
 
 
 if(isset($_GET["sec"])){
@@ -16,32 +16,32 @@ if(isset($_GET["sec"])){
 }
 ?>
 
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/covid-data/style.css?v=1">
+	<link rel="stylesheet" href="/covid-data/style.css?v=1">
 
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/covid-data/js/functions.js"></script>
+    <script type="text/javascript" src="/covid-data/js/functions.js"></script>
 
 	<section  id="covid">
 		<div class="">
 
 
-			<?php include get_theme_file_path( '/covid-data/head.php' );?>
+			<?php include ( '/covid-data/head.php' );?>
 
 
 			<div class="">
 				<?php if($section=="datos"){?>
 					<div class="">
-						<?php include get_theme_file_path( '/covid-data/page-datos.php' );?>
+						<?php include ( '/covid-data/page-datos.php' );?>
 					</div>
 				<?php }?>
 				<?php if($section=="preguntas"){?>
 					<div class="">
-						<?php include get_theme_file_path( '/covid-data/page-faqs.php' );?>
+						<?php //include ( '/covid-data/page-faqs.php' );?>
 					</div>
 				<?php }?>
 				<?php if($section=="vacunas"){?>
 					<div class="">
-						<?php include get_theme_file_path( '/covid-data/page-vacunas.php' );?>
+						<?php //include ( '/covid-data/page-vacunas.php' );?>
 					</div>
 				<?php }?>
 
@@ -52,4 +52,4 @@ if(isset($_GET["sec"])){
 	</section>
 
 <?php
-get_footer();
+//get_footer();
